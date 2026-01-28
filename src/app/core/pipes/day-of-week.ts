9214381cb9199e20c48ff14386aac323e32c977d
Shahriyor <rshahriyor@Mac.mra.com>
2026-01-28 06:48:50 -0500

    worked on company detail data

---
+++ src/app/core/pipes/day-of-week.ts
@@ -0,0 +1,15 @@
+export const dayOfWeek = (day?: number): string => {
+    if (!day) return ''
+
+    const days: Record<number, string> = {
+        1: 'Понедельник',
+        2: 'Вторник',
+        3: 'Среда',
+        4: 'Четверг',
+        5: 'Пятница',
+        6: 'Суббота',
+        7: 'Воскресенье'
+    }
+
+    return days[day] ?? ''
+}
